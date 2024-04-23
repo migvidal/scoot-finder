2a67cb2108031f5c6598e037735ebcfdbcfbac3b
migvidal <mvidal@air-institute.com>
2024-04-23 18:46:13 +0200

    Install Tailwind and remove boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Find your perfect scooter</h1>
+      <a href="#" className='text-blue-600'>Link</a>
+    </>
+  )
+}
+
+export default App

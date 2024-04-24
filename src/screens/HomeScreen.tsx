a4a709b1ad6ba5b2816717df4d210417fb8c65ab
migvidal <mvidal@air-institute.com>
2024-04-24 15:53:26 +0200

    Prettify file

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from "react";
 import RoundedButton from "../RoundedButton";
 
 function HomeScreen({ children }: { children: ReactNode }) {
-    return (
-        <div className='flex flex-col items-center'>
-            <h1 className='text-6xl font-bold text-center px-4'>Find your perfect scooter<span className='text-yellow-600'>!</span></h1>
-            <div className='py-10'></div>
-            {children}
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-6xl font-bold text-center px-4">
+        Find your perfect scooter<span className="text-yellow-600">!</span>
+      </h1>
+      <div className="py-10"></div>
+      {children}
+    </div>
+  );
 }
-export default HomeScreen
+export default HomeScreen;

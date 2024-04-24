eda355d1a74ac0a78c9cd3b9ffb03e43a893f0c2
migvidal <mvidal@air-institute.com>
2024-04-24 15:50:40 +0200

    Format code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import RoundedButton from './RoundedButton'
-import HomeScreen from './screens/HomeScreen'
-import WeightScreen from './screens/WeightScreen'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import RoundedButton from "./RoundedButton";
+import HomeScreen from "./screens/HomeScreen";
+import WeightScreen from "./screens/WeightScreen";
 
 function App() {
-
   return (
     <BrowserRouter>
       <div>
@@ -12,21 +11,22 @@ function App() {
       </div>
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <HomeScreen>
-              <Link to="/weight"
-                className='rounded-md rounded-tr-2xl rounded-br-2xl bg-yellow-300 dark:bg-yellow-600 px-4 py-2'
-                >Begin
+              <Link
+                to="/weight"
+                className="rounded-md rounded-tr-2xl rounded-br-2xl bg-yellow-300 dark:bg-yellow-600 px-4 py-2"
+              >
+                Begin
               </Link>
             </HomeScreen>
           }
-        >
-        </Route>
-        <Route path='/weight' element={<WeightScreen />}></Route>
+        />
+        <Route path="/weight" element={<WeightScreen />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/screens/WeightScreen.tsx
+++ src/screens/WeightScreen.tsx
@@ -1,29 +1,32 @@
-import { ChangeEvent, ChangeEventHandler, FormEvent, useState } from "react"
+import { ChangeEvent, ChangeEventHandler, FormEvent, useState } from "react";
 
 function WeightScreen() {
-    const [weight, setWeight] = useState(0)
-    function onFormSubmit(e: FormEvent) {
-        e.preventDefault()
-    }
-    function onWeightRangeChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const currentValue = e.currentTarget.value
-        setWeight(Number(currentValue))
-    }
-    return (
-        <div className='flex flex-col items-center'>
-            <h1 className='text-6xl font-bold text-center px-4'>How much do you weigh?</h1>
-            <p>{weight}</p>
-            <form onSubmit={onFormSubmit}>
-                <input 
-                type="range"
-                name="weight-range" 
-                id="weight-range" 
-                value={weight + 'kg'} 
-                onChange={onWeightRangeChange}
-                min="20"
-                max="200"/>
-            </form>
-        </div>
-    )
+  const [weight, setWeight] = useState(0);
+  function onFormSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+  function onWeightRangeChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const currentValue = e.currentTarget.value;
+    setWeight(Number(currentValue));
+  }
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-6xl font-bold text-center px-4">
+        How much do you weigh?
+      </h1>
+      <p>{weight}</p>
+      <form onSubmit={onFormSubmit}>
+        <input
+          type="range"
+          name="weight-range"
+          id="weight-range"
+          value={weight + "kg"}
+          onChange={onWeightRangeChange}
+          min="20"
+          max="200"
+        />
+      </form>
+    </div>
+  );
 }
-export default WeightScreen
+export default WeightScreen;

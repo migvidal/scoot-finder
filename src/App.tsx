bbf6eb0848fd52efdb415d3067dc3e6cb4007b09
migvidal <mvidal@air-institute.com>
2024-04-24 12:26:37 +0200

    Add click handler to custom button

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className='flex flex-col items-center'>
       <h1 className='text-6xl font-bold text-center px-4'>Find your perfect scooter<span className='text-yellow-600'>!</span></h1>
       <div className='py-10'></div>
-      <RoundedButton>Start the survey</RoundedButton>
+      <RoundedButton onClick={() => { }}>Start the survey</RoundedButton>
     </div>
   )
 }
--- src/RoundedButton.tsx
+++ src/RoundedButton.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from "react"
 
-function RoundedButton({ children }: { children: ReactNode }) {
+function RoundedButton({ onClick, children }: { onClick: () => any, children: ReactNode }) {
     return (
-        <button className="rounded-md rounded-tr-2xl rounded-br-2xl bg-yellow-300 dark:bg-yellow-600 px-4 py-2">
+        <button
+            className="rounded-md rounded-tr-2xl rounded-br-2xl bg-yellow-300 dark:bg-yellow-600 px-4 py-2"
+            onClick={onClick}
+        >
             {children}
         </button>
     )

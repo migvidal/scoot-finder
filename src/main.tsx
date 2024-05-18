400c816bcebb02dc6739c00622780ee92eb6a590
migvidal <mvidal@air-institute.com>
2024-05-18 13:19:28 +0200

    Add Talkr. Create basic i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import en from "./i18n/en.json";
+import es from "./i18n/es.json";
+import { Talkr } from "talkr";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Talkr languages={{en, es}} defaultLanguage={"en"}>
+      <App />
+    </Talkr>
+  </React.StrictMode>
+);
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from "react";
 import RoundedButton from "../components/RoundedButton";
+import { useT } from "talkr";
 
 function HomeScreen({ children }: { children: ReactNode }) {
+  const { T } = useT();
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-6xl font-bold text-center px-4">
-        Find your perfect scooter<span className="text-yellow-600">!</span>
+        {T("find-your-perfect-scooter")}
+        <span className="text-yellow-600">!</span>
       </h1>
       <div className="py-10"></div>
       {children}

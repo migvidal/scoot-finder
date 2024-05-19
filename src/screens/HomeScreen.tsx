08c3dc7b2902222e01dc2a25a50dd6e9bce3c290
migvidal <mvidal@air-institute.com>
2024-05-19 18:35:56 +0200

    Integrate logo better in home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -10,9 +10,9 @@ function HomeScreen({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col items-center">
       <div className="w-full flex flex-wrap sm:flex-nowrap sm:flex-row-reverse items-center justify-center">
-        <ExtendedLogo className="h-full w-80 mb-4" />
-        <div className="mb-4">
-          <h1 className="text-6xl font-bold max-w-sm">
+        <ExtendedLogo className="h-full w-80 mb-4 sm:-ml-20" />
+        <div className="mb-4 max-w-sm">
+          <h1 className="text-6xl font-bold">
             {T("find-your-perfect-scooter")}
           </h1>
           <p className="text-xl font-bold py-4">

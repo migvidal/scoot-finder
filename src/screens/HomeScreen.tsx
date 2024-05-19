89a86448423a3f1b4b525eb5613da9e6ba7ea18a
migvidal <mvidal@air-institute.com>
2024-05-19 18:28:25 +0200

    Add subtitle

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -10,10 +10,15 @@ function HomeScreen({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col items-center">
       <div className="w-full flex flex-wrap sm:flex-nowrap sm:flex-row-reverse items-center justify-center">
-        <ExtendedLogo className="h-full w-80 mb-4"/>
-        <h1 className="text-6xl font-bold mb-4 px-4 text-center max-w-sm">
-          {T("find-your-perfect-scooter")}
-        </h1>
+        <ExtendedLogo className="h-full w-80 mb-4" />
+        <div className="mb-4">
+          <h1 className="text-6xl font-bold max-w-sm">
+            {T("find-your-perfect-scooter")}
+          </h1>
+          <p className="text-xl font-bold py-4">
+            {T("answer-a-few-questions")}
+          </p>
+        </div>
       </div>
       {children}
     </div>

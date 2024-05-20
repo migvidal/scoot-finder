4011bafebe7a0c0e606268ebb2cc03c40cb1a312
migvidal <mvidal@air-institute.com>
2024-05-20 18:19:36 +0200

    Change vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/scoot-finder/",
+  plugins: [react()],
+})
+
